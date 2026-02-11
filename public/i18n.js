6ea200f488c18aebbf8837a45ddeddf985589237
TakkunMC_jp <Kaneda.family.803@gmail.com>
2026-02-11 10:20:26 +0900

    Delete public directory

--- public/i18n.js
+++
@@ -1,5 +0,0 @@
-const i18n={
- ja:{title:"GlobalBAN Network",invite:"BOTを招待",login:"Discordでログイン"},
- en:{title:"GlobalBAN Network",invite:"Invite BOT",login:"Login with Discord"}
-};
-function t(k){return i18n[localStorage.lang||"ja"][k];}
